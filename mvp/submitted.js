85e8c08f91e0719540d098d28dd5f16d321413ec
Fatou Kebe <fkebe704@gmail.com>
2019-08-05 15:25:43 -0400

    yes

--- mvp/submitted.js
+++ mvp/submitted.js
@@ -4,7 +4,21 @@ console.log(urlParams.has('ans1')); // true
 console.log(urlParams.get('ans1')); // "edit"
 
 console.log(urlParams.has('ans2')); // true
-console.log(urlParams.get('ans2'));
+var one = urlParams.get('ans1');
+var two = urlParams.get('ans2');
+var three = urlParams.get('ans3');
+var four = urlParams.get('ans4');
+var five = urlParams.get('ans5');
+var six = urlParams.get('ans6');
+var seven = urlParams.get('ans7');
+var eight = urlParams.get('ans8');
+var nine = urlParams.get('ans9');
+var ten = urlParams.get('ans10');
 
+var answers = "";
+var i;
+for (i = 0; i < 10; i++) {
+  answers +=  ;
+}
 // console.log(urlParams.has('ans1')); // true
 // console.log(urlParams.get('ans1'));
