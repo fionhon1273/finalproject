1134f2e25669d31a6b00397f86005f72bbc12134
Fatou Kebe <fkebe704@gmail.com>
2019-08-06 10:12:35 -0400

    hello

--- mvp/submitted.js
+++ mvp/submitted.js
@@ -1,31 +1,16 @@
 var urlParams = new URLSearchParams(window.location.search);
 
-console.log(urlParams.has('ans1')); // true
-console.log(urlParams.get('ans1')); // "edit"
-
-console.log(urlParams.has('ans2')); // true
-var one = urlParams.get('ans1');
-var two = urlParams.get('ans2');
-var three = urlParams.get('ans3');
-var four = urlParams.get('ans4');
-var five = urlParams.get('ans5');
-var six = urlParams.get('ans6');
-var seven = urlParams.get('ans7');
-var eight = urlParams.get('ans8');
-var nine = urlParams.get('ans9');
-var ten = urlParams.get('ans10');
-
-
-
-var answers = "";
 var i;
-for (i = 0; i =< 10; i++) {
-  answers +=  ;
+var answers = "";
+for(i = 1; i <= 10; i++) {
+  answers += urlParams.get(`ans${i}`);
 }
 
+console.log(answers);
 
-
-var results= {}
-results = {"babbaaaaab":"Calista", "bbbaaabaab":"Mame Diarra", "aabbaaabab":"Ngina", "ababaabaaa":"Nicole M", "baaabbabab":"Izear", "abbbbbbaab":"Mariela", "abaaaaaaab":"Eva", "ababaabaab":"Fion", "aaaaababbb":"Fatou", "bbbbbbaabb":"Shira", "bbaaaabaaa":"Dana", "bbbaaaaaba":"Syeda", "bbbaaabbbb":"Judith", "babbabaaab":"Erika", "aaaaaaaaab":"Nicole I", "bbbbaaaabb":"Maeghan", "bbbbbababb":"Andrea", "bbbbaababb":"Amelia", "babaaaaabb":"Liana", "abaabbababb":"Yi Yang"}
+var results= {"babbaaaaab":"Calista", "bbbaaabaab":"Mame Diarra", "aabbaaabab":"Ngina", "ababaabaaa":"Nicole M", "baaabbabab":"Izear", "abbbbbbaab":"Mariela", "abaaaaaaab":"Eva", "ababaabaab":"Fion", "aaaaababbb":"Fatou", "bbbbbbaabb":"Shira", "bbaaaabaaa":"Dana", "bbbaaaaaba":"Syeda", "bbbaaabbbb":"Judith", "babbabaaab":"Erika", "aaaaaaaaab":"Nicole I", "bbbbaaaabb":"Maeghan", "bbbbbababb":"Andrea", "bbbbaababb":"Amelia", "babaaaaabb":"Liana", "abaabbababb":"Yi Yang"}
 // console.log(urlParams.has('ans1')); // true
 // console.log(urlParams.get('ans1'));
+if (answers in results){
+  console.log(results[answers]);
+}
