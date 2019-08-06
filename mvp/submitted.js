32c4de1de603526f992c8c39ffe73489ca8cee52
Fatou Kebe <fkebe704@gmail.com>
2019-08-06 10:38:44 -0400

    ok

--- mvp/submitted.js
+++ mvp/submitted.js
@@ -11,9 +11,3 @@ console.log(answers);
 var results= {"babbaaaaab":"Calista", "bbbaaabaab":"Mame Diarra", "aabbaaabab":"Ngina", "ababaabaaa":"Nicole M", "baaabbabab":"Izear", "abbbbbbaab":"Mariela", "abaaaaaaab":"Eva", "ababaabaab":"Fion", "aaaaababbb":"Fatou", "bbbbbbaabb":"Shira", "bbaaaabaaa":"Dana", "bbbaaaaaba":"Syeda", "bbbaaabbbb":"Judith", "babbabaaab":"Erika", "aaaaaaaaab":"Nicole I", "bbbbaaaabb":"Maeghan", "bbbbbababb":"Andrea", "bbbbaababb":"Amelia", "babaaaaabb":"Liana", "abaabbababb":"Yi Yang"}
 // console.log(urlParams.has('ans1')); // true
 // console.log(urlParams.get('ans1'));
-if (answers in results){
-  console.log(results[answers]);
-  document.getElementById("result").innerHTML = (results[answers])
-} else{
-  
-}
