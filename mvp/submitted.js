fb8d2fae0410aca884f01efac744bb343ddc39c9
Fatou Kebe <fkebe704@gmail.com>
2019-08-06 11:25:02 -0400

    fixeddddd

--- mvp/submitted.js
+++ mvp/submitted.js
@@ -11,18 +11,31 @@ console.log(answers);
 var results= {"babbaaaaab":"Calista", "bbbaaabaab":"Mame Diarra", "aabbaaabab":"Ngina", "ababaabaaa":"Nicole M", "baaabbabab":"Izear", "abbbbbbaab":"Mariela", "abaaaaaaab":"Eva", "ababaabaab":"Fion", "aaaaababbb":"Fatou", "bbbbbbaabb":"Shira", "bbaaaabaaa":"Dana", "bbbaaaaaba":"Syeda", "bbbaaabbbb":"Judith", "babbabaaab":"Erika", "aaaaaaaaab":"Nicole I", "bbbbaaaabb":"Maeghan", "bbbbbababb":"Andrea", "bbbbaababb":"Amelia", "babaaaaabb":"Liana", "abaabbababb":"Yi Yang"}
 // console.log(urlParams.has('ans1')); // true
 // console.log(urlParams.get('ans1'));
-<<<<<<< HEAD
-=======
+
 if (answers in results){
   console.log(results[answers]);
-  document.getElementById("result").innerHTML = (results[answers])
-} else{
-    for(answers in results) {
-      var value = results[answers];
-      document.getElementById("result").innerHTML = value
-    }
-
+  document.getElementById("result").innerHTML = results[answers];
 }
+else{
+    var rand = Object.values(results);
+    document.getElementById("result").innerHTML = rand[Math.floor(Math.random() * rand.length)];
+}
+
+//
+// if (answers in results){
+//   console.log(results[answers]);
+//   document.getElementById("result").innerHTML = (results[answers]);
+// } else{
+//   for(i = 1; i <= 10; i++) {
+//     document.getElementById("result").innerHTML = (results[`ans${i}`]);
+//   }
+// }
+//
+// var randomProperty = function (results) {
+//   var keys = Object.keys(object);
+//   return object[keys[Math.floor(keys.length * Math.random())]];
+// };
+
 //
 // for(answers in results) {
 //   var value = results[answers];
@@ -34,4 +47,3 @@ if (answers in results){
 // for( var i=0; i < anysize; i++ )
 //         result += results[Math.floor(Math.random() * results.length)];
 // console.log(result);
->>>>>>> a6c77292eded98de458af4db3554f5269c7f8f45
