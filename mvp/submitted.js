1532a7cdfb77c86e31b2cc565f9ab5c80bff8e05
Fatou Kebe <fkebe704@gmail.com>
2019-08-07 09:33:02 -0400

    yeah

--- mvp/submitted.js
+++ mvp/submitted.js
@@ -9,14 +9,19 @@ for(i = 1; i <= 10; i++) {
 console.log(answers);
 
 var results= {"babbaaaaab":"Calista", "bbbaaabaab":"Mame Diarra", "aabbaaabab":"Ngina", "ababaabaaa":"Nicole M", "baaabbabab":"Izear", "abbbbbbaab":"Mariela", "abaaaaaaab":"Eva", "ababaabaab":"Fion", "aaaaababbb":"Fatou", "bbbbbbaabb":"Shira", "bbaaaabaaa":"Dana", "bbbaaaaaba":"Syeda", "bbbaaabbbb":"Judith", "babbabaaab":"Erika", "aaaaaaaaab":"Nicole I", "bbbbaaaabb":"Maeghan", "bbbbbababb":"Andrea", "bbbbaababb":"Amelia", "babaaaaabb":"Liana", "abaabbababb":"Yi Yang", "abbaaababb":"Etta", "abbbbbbaba":"Connie"}
-// console.log(urlParams.has('ans1')); // true
-// console.log(urlParams.get('ans1'));
+
+var details = {"Calista":{"image":"../img/calista.jpeg", "descript":"This person is short."},"Mame Diarra":{"image":"", "descript":""},"Ngina":{"image":"", "descript":""},"Nicole M":{"image":"", "descript":""},"Izear":{"image":"", "descript":""},"Mariela":{"image":"", "descript":""},"Eva":{"image":"", "descript":""},"Fion":{"image":"", "descript":""},"Fatou":{"image":"", "descript":""},"Shira":{"image":"", "descript":""},"Dana":{"image":"", "descript":""},"Syeda":{"image":"", "descript":""},"Judith":{"image":"", "descript":""},"Erika":{"image":"", "descript":""},"Nicole I":{"image":"", "descript":""},"Maeghan":{"image":"", "descript":""},"Andrea":{"image":"", "descript":""},"Amelia":{"image":"", "descript":""},"Liana":{"image":"", "descript":""},"Yi Yang":{"image":"", "descript":""},"Etta":{"image":"", "descript":""},"Connie":{"image":"", "descript":""}}
 
 if (answers in results){
   console.log(results[answers]);
   document.getElementById("result").innerHTML = results[answers];
+  var person = results[answers];
+  var deets = details[person];
+  document.getElementById("image").setAttribute("src", deets["image"]);
+  document.getElementById("description").innerText = deets["descript"];
 }
 else{
     var rand = Object.values(results);
     document.getElementById("result").innerHTML = rand[Math.floor(Math.random() * rand.length)];
+    
 }
